1c9fffa1ec754b0d1c3c18e0d1ed82522479b378
nicolasizb <nicolasizquierdocontacto@gmail.com>
2023-12-07 21:54:08 -0500

    ch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,11 @@ export class AppComponent {
       img: "../assets/images/logo-name-image.png",
       description: "It is a platform specialized in organizing events that simplifies the planning and execution of local community experiences.",
       link: "https://event-hub-delta.vercel.app/"
+    },
+    {
+      img: "../assets/images/logo-amp.png",
+      description: "Comprehensive solution designed by a leading company with business verticals in renewable energy, swimming pools and asset management.",
+      link: "https://website-amp.vercel.app/"
     }
   ]
 
